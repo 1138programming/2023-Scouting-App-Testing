789cf921266b356b2ca33d32bb52a2381f2d1570
Khandanger <111544394+Glutinfree@users.noreply.github.com>
2023-10-26 23:15:35 -0700

    Initial Commit.

---
+++ Login.ts
@@ -0,0 +1,12 @@
+
+import { Boot } from 'Boot';
+import newWin;
+
+
+function LoginCheck(input) {
+
+    if (input == "abcd") {
+        var ident = newWin.document.getElementById('index.html');
+        ident.href = 'main.html';
+    }
+}
