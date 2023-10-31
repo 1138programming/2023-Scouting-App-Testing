c69aa233b6d876a3c9bd9a9959019c98d3bdde2b
Khandanger <111544394+Glutinfree@users.noreply.github.com>
2023-10-30 23:12:46 -0700

    it works

--- Login.ts
+++ Login.ts
@@ -1,12 +1,9 @@
 
-import { Boot } from 'Boot';
-import newWin;
 
 
 function LoginCheck(input) {
-
     if (input == "abcd") {
-        var ident = newWin.document.getElementById('index.html');
-        ident.href = 'main.html';
+        
+        window.location.href = "main.html";
     }
 }
