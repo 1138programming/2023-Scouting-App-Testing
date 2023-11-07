146dc8cb45738137d47278e74d868981701291fe
Goomisica <60203441+Goomisica@users.noreply.github.com>
2023-11-06 22:51:37 -0800

    rotation not working on settings cog; need to revisit.

---
+++ src/html/clickrotate.ts
@@ -0,0 +1,14 @@
+(function(document){
+    var icon = document.getElementById('settings-button');
+    var open = false;
+    
+    icon.addEventListener('click', function(){
+      if(open){
+        icon.className = 'topRightWithOffset settings-normal';  
+      } else{
+        icon.className = 'topRightWithOffset settings-rotated';
+      }
+      
+      open = !open;
+    });
+  })(document);
